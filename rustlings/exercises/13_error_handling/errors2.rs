0086071ce99186917d668cb8f6a250500759e6a3
CaffeinePwrdAl <ajw.walters@gmail.com>
2025-07-20 23:56:05 +0100

    Some more rustlings done

--- rustlings/exercises/13_error_handling/errors2.rs
+++ rustlings/exercises/13_error_handling/errors2.rs
@@ -19,11 +19,15 @@ use std::num::ParseIntError;
 fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
+    let qty = item_quantity.parse::<i32>()?;
+    Ok(qty *cost_per_item + processing_fee)
 
-    // TODO: Handle the error case as described above.
-    let qty = item_quantity.parse::<i32>();
-
-    Ok(qty * cost_per_item + processing_fee)
+    // OR
+    //
+    //match item_quantity.parse::<i32>() {
+    //    Ok(num) => Ok(num * cost_per_item + processing_fee),
+    //    Err(s) => Err(s),
+    //}
 }
 
 fn main() {
